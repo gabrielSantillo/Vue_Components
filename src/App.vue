33b859dc3ea604eae9862ab6759176b428256034
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-08-06 15:14:21 -0600

    Finished my knowledge check

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <page-header></page-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PageHeader from './components/PageHeader.vue'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
+    PageHeader,
+  },
 }
 </script>
 
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h3>This is my header</h3>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: `page-header`
+    }
+</script>
+
+<style scoped>
+h3 {
+    color: blue;
+}
+</style>
